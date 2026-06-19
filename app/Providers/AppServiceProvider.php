2a24db2537a6fedf3d9d2fed5c2f97ef7321de18
kzxazka <atqiyaazka96@gmail.com>
2026-06-20 04:59:21 +0700

    Security: Enforce HTTPS for non-local environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
 
+        if (env('APP_ENV') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \App\Models\Project::observe(\App\Observers\ProjectObserver::class);
         \App\Models\Task::observe(\App\Observers\TaskObserver::class);
         \App\Models\Transaction::observe(\App\Observers\TransactionObserver::class);
